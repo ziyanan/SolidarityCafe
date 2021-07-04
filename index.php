6502bf8a9734a6bc8153827bfb5ba78c6d88d20f
WAngieZhao <47250853+WAngieZhao@users.noreply.github.com>
2021-07-04 12:23:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
   <div class="wrapper">
     <section class="form signup">
-      <header>Realtime Chat App - GirlsTalk</header>
+      <header>Solidarity Cafe</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
